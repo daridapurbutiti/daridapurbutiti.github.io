90ea4f1d70efe4cf3d4063d2690c84865a2b59f3
daridapurbutiti <daridapurbutiti@gmail.com>
2017-05-16 11:32:49 +0700

    first commit

---
+++ js/app.js
@@ -0,0 +1,44 @@
+// When the DOM is ready, run this function
+$(document).ready(function() {
+  //Set the carousel options
+  $('#quote-carousel').carousel({
+    pauseOnHover: true,
+    interval: 3000,
+  });
+
+  $(window).scroll(function () {
+    var top = $(document).scrollTop();
+    $('.splash').css({
+      'background-position': '0px -'+(top/3).toFixed(2)+'px'
+    });
+
+    /*if(top > 50)
+      $('#home > .navbar').removeClass('navbar-transparent');
+    else
+      $('#home > .navbar').addClass('navbar-transparent');*/
+  });
+
+  // star rating
+  /*var $star_rating = $('.star-rating .fa');
+
+  var SetRatingStar = function() {
+    return $star_rating.each(function() {
+      if (parseInt($star_rating.siblings('input.rating-value').val()) >= parseInt($(this).data('rating'))) {
+        return $(this).removeClass('fa-star-o').addClass('fa-star');
+      } else {
+        return $(this).removeClass('fa-star').addClass('fa-star-o');
+      }
+    });
+  };
+
+  $star_rating.on('click', function() {
+    $star_rating.siblings('input.rating-value').val($(this).data('rating'));
+    return SetRatingStar();
+  });
+
+  SetRatingStar();
+  $(document).ready(function() {
+
+  });*/
+
+});
